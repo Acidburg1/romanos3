efc62ffc697d714a4ab21e04e55efc899b9c8cff
David Rovirosa <davidrovirosa@gmail.com>
2014-05-31 14:43:04 +0200

    hasta los que tienen 2 cifras

--- src/Romanos3/Romanos3.php
+++ src/Romanos3/Romanos3.php
@@ -8,6 +8,8 @@ namespace Romanos3;
  * and open the template in the editor.
  */
 
+use Romanos3\Tansformaciones;
+
 /**
  * Description of Romanos3
  *
@@ -17,121 +19,37 @@ class Romanos3 {
 
     private static $basicos = array('I', 'II', 'III', 'IV', 'V', 'VI',
         'VII', 'VIII', 'IX', 'X');
+    private static $transformaciones = array(array('I', 'V'),
+        array('X', 'L')
+    );
 
     public function devuelve($numero) {
-        //return self::$basicos[$numero - 1];
         $devuelve = "";
-//        $digito = $this->getDigit($numero, 0);
-//        //$devuelve = self::$basicos[$numero - 1];
-        print("longitud=" . strlen($numero) . "\n");
-        print("digito=" . $digito = $this->getDigit($numero, 0) . "\n");
-        print("numero =" . $numero . "\n");
-        for ($a = 0; $a != strlen($numero); $a++) {
-
-            $digito = $this->getDigit($numero, $a);
+        $cantidadDeNumeros = strlen($numero);
+        for ($posicion = 0; $posicion != $cantidadDeNumeros; $posicion++) {
+            $digito = $this->getDigit($numero, $posicion);
             if ($digito != 0) {
-                if ($a == 0) {
-                    $devuelve = self::$basicos[$digito - 1] . $devuelve;
-                }
-                if ($a == 1) {
-                    $devuelve1 = self::$basicos[$digito - 1];
-                    $devuelve = str_replace('I', 'X', $devuelve1) . $devuelve;
-                }
-                if ($a == 1) {
-                    $devuelve1 = self::$basicos[$digito - 1];
-                    $devuelve = str_replace('I', 'X', $devuelve1) . $devuelve;
-                }
+                $devuelve1 = $this->devuelveDigito($digito);
+                $digitoTranformado = $this->devuelveDigitoTranformado($devuelve1, $posicion);
+                $devuelve = $digitoTranformado . $devuelve;
             }
         }
         return $devuelve;
-        ////return "I";
-//        $devuelve = "";
-        // $digito = $this->getLastDigit($numero);
-//        $limite = $this->tocaLimite($digito, 0);
-        //return $this->devuelveCifra($digito);
+    }
+
+    private function devuelveDigito($digito) {
+        return self::$basicos[$digito - 1];
+    }
+
+    private function devuelveDigitoTranformado($numero, $posicion) {
+        $numero = str_replace('I', self::$transformaciones[$posicion][0], $numero);
+        $numero = str_replace('V', self::$transformaciones[$posicion][1], $numero);
+        return $numero;
     }
 
     private function getDigit($num, $numero2) {
         $digito = strlen($num) - 1 - $numero2;
-        //print("numero en getdigit=" . $digito . "\n");
-
         return (Integer) $num[$digito];
-    }
-
-    public function devuelveRepetidos($digito) {
-        $devuelve = "";
-        for ($a = 0; $a < $this->contador($digito); $a++) {
-            $devuelve = $devuelve . 'I';
-        }
-        return $devuelve;
-    }
-
-    public function devuelveCifra($digito) {
-        $devuelve = $this->devuelveRepetidos($digito);
-        $lugarEscribo = $this->derechaIzquierda($digito);
-        $numeroqueEscribir = "";
-        if ($digito > 3) {
-            $numeroqueEscribir = $this->cogerLimite($digito);
-        }
-        if ($lugarEscribo == 'I') {
-            $devuelve = $devuelve . $numeroqueEscribir;
-        }
-        if ($lugarEscribo == 'D') {
-            $devuelve = $numeroqueEscribir . $devuelve;
-        }
-        return $devuelve;
-    }
-
-    public function cogerLimite($digito) {
-        if ($digito == 9) {
-            return 'X';
-        } else {
-            return 'V';
-        }
-    }
-
-    public function derechaIzquierda($digito) {
-        if ($digito == 9) {
-            return 'I';
-        }
-
-        if ($digito > 5) {
-            return 'D';
-        } else {
-            return 'I';
-        }
-    }
-
-    public function contador($digito) {
-        if ($this->tocaLimite($digito) !== 0) {
-            return 1;
-        }
-        if ($digito == 5) {
-            return 0;
-        }
-
-
-        $contador = 0;
-        while ($digito > 5 + $contador) {
-            $contador++;
-        }
-
-        if ($digito < 5) {
-            $contador = $digito;
-        }
-        return $contador;
-    }
-
-    public function tocaLimite($digito) {
-
-        if ($digito + 1 == 5 || $digito - 1 == 5) {
-            return 5;
-        }
-        if ($digito + 1 == 10) {
-
-            return 10;
-        }
-        return 0;
     }
 
 //put your code here
--- src/Romanos3/Test/Romanos3Test.php
+++ src/Romanos3/Test/Romanos3Test.php
@@ -32,78 +32,6 @@ class Romanos3Test extends \PHPUnit_Framework_TestCase {
         $this->assertTrue(true);
     }
 
-    public function testContador1() {
-        $this->assertEquals(1, $this->romanos3->contador("1"));
-    }
-
-    public function testContador2() {
-        $this->assertEquals(2, $this->romanos3->contador("2"));
-    }
-
-    public function testContador3() {
-        $this->assertEquals(3, $this->romanos3->contador("3"));
-    }
-
-    public function testContador4() {
-        $this->assertEquals(1, $this->romanos3->contador("4"));
-    }
-
-    public function testContador5() {
-        $this->assertEquals(0, $this->romanos3->contador("5"));
-    }
-
-    public function testContador6() {
-        $this->assertEquals(1, $this->romanos3->contador("6"));
-    }
-
-    public function testContador7() {
-        $this->assertEquals(2, $this->romanos3->contador("7"));
-    }
-
-    public function testContador8() {
-        $this->assertEquals(3, $this->romanos3->contador("8"));
-    }
-
-    public function testContador9() {
-        $this->assertEquals(1, $this->romanos3->contador("9"));
-    }
-
-    public function testTocaLimite1() {
-        $this->assertEquals('0', $this->romanos3->tocaLimite("1"));
-    }
-
-    public function testTocaLimite2() {
-        $this->assertEquals('0', $this->romanos3->tocaLimite("2"));
-    }
-
-    public function testTocaLimite3() {
-        $this->assertEquals('0', $this->romanos3->tocaLimite("3"));
-    }
-
-    public function testTocaLimite4() {
-        $this->assertEquals('5', $this->romanos3->tocaLimite("4"));
-    }
-
-    public function testTocaLimite5() {
-        $this->assertEquals('0', $this->romanos3->tocaLimite("5"));
-    }
-
-    public function testTocaLimite6() {
-        $this->assertEquals('5', $this->romanos3->tocaLimite("6"));
-    }
-
-    public function testTocaLimite7() {
-        $this->assertEquals('0', $this->romanos3->tocaLimite("7"));
-    }
-
-    public function testTocaLimite8() {
-        $this->assertEquals('0', $this->romanos3->tocaLimite("8"));
-    }
-
-    public function testTocaLimite9() {
-        $this->assertEquals('10', $this->romanos3->tocaLimite("9"));
-    }
-
     public function testDevuelve1() {
         $this->assertEquals('I', $this->romanos3->devuelve("1"));
     }
@@ -152,6 +80,10 @@ class Romanos3Test extends \PHPUnit_Framework_TestCase {
 
     public function testDevuelve15() {
         $this->assertEquals('XV', $this->romanos3->devuelve("15"));
+    }
+
+    public function testDevuelve55() {
+        $this->assertEquals('LV', $this->romanos3->devuelve("55"));
     }
 
 }
